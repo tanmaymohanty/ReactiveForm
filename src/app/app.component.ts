ad124fc15bc002c7325068fd5343c35550679a05
tanmaymohanty <stmohanty1@gmail.com>
2019-03-18 22:03:34 +0530

    Commit the first Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'ReactiveForm';
+  addCondoPhaseForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) {
+  }
+  ngOnInit() {
+    // For validation
+    this.createAddCondoPhaseForm();
+  }
+
+  createAddCondoPhaseForm() {
+    this.addCondoPhaseForm = this.fb.group({
+      condoPhase: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.maxLength(3),
+        // Validators.pattern(this.common.ALPHANUMERIC_REGEX)
+      ])
+    });
+  }
 }
